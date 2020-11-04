8e27e42ac2e3cbc5cc3cacb71d0c6c9916af118c
saipulanwarr <saipul0994@gmail.com>
2020-11-04 23:09:36 +0700

    make the onboarding screen one

---
+++ app/src/main/java/com/sa/samov/onboarding/OnboardingOne.kt
@@ -0,0 +1,12 @@
+package com.sa.samov.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sa.samov.R
+
+class OnboardingOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_one)
+    }
+}
