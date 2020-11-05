28506b7db4c8c1f19abf9ba6043d93fc46100556
saipulanwarr <saipul0994@gmail.com>
2020-11-05 20:52:02 +0700

    make the bench select screen

---
+++ app/src/main/java/com/sa/samov/PilihBangkuActivity.kt
@@ -0,0 +1,11 @@
+package com.sa.samov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PilihBangkuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pilih_bangku)
+    }
+}
