86ae62207c785025f03af1dc4b171143acac8d58
saipulanwarr <saipul0994@gmail.com>
2020-11-09 15:54:21 +0700

    setting firebase

--- app/src/main/java/com/sa/samov/sign/SignIn.kt
+++ app/src/main/java/com/sa/samov/sign/SignIn.kt
@@ -1,12 +1,24 @@
 package com.sa.samov.sign
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 import com.sa.samov.R
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        button2.setOnClickListener {
+            // Write a message to the database
+            // Write a message to the database
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("message")
+
+            myRef.setValue("Hello, World!")
+        }
     }
 }
